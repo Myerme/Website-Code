d21da3885b8176c44af74a2ebca1e62b9b07965f
Myerme <111097141+Myerme@users.noreply.github.com>
2022-11-01 00:21:19 +0000

    Add files via upload

---
+++ Bored_Lofi_Kinda_code/wordsss.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+
+
+inp="abcdefghijklmnopqrstuvwxyz"
+letter = [x for x in inp]
+for y in list(permutations(letter)):
+    print("".join(y))
+
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
+    #DONT RUN
